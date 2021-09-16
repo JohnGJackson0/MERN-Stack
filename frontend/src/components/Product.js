7d0166d62a167677f550508d0edf4610fdb2c981
jgjio <johngjackson0@gmail.com>
2021-09-16 17:54:55 -0400

    Implement react router

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,29 +1,31 @@
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
-        <Card.Body>
-          <a href={`/product/${product._id}`}>
-            <Card.Title as="div">
-              <strong>{product.name}</strong>
-            </Card.Title>
-          </a>
-          <Card.Text as="div">
-            <Rating
-              value={product.rating}
-              text={`${product.numReviews} reviews`}
-            />
-            <div className="my-3">
-              {product.rating} from {product.numReviews} reviews
-            </div>
-          </Card.Text>
-          <Card.Text as="h3">${product.price}</Card.Text>
-        </Card.Body>
-      </a>
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+          <div className="my-3">
+            {product.rating} from {product.numReviews} reviews
+          </div>
+        </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
     </Card>
   );
 };
