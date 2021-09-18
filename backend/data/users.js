9829406f446abce200911781c98e10bb20ebaa0b
jgjio <johngjackson0@gmail.com>
2021-09-18 11:04:53 -0400

    Prepare stubbed data for DB import via seeder scripts

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("password", 10),
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "John@example.com",
+    //npm i bcryptjs to hash
+    password: bcrypt.hashSync("password", 10),
+  },
+  {
+    name: "Jane Doe",
+    email: "Jane@example.com",
+    password: bcrypt.hashSync("password", 10),
+  },
+];
+
+export default users;
